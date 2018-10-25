fabac6b207cca3049908816e254887aeb4b2083a
KaptainGoku <vuthongcs@gmail.com>
2018-10-25 16:15:26 +0700

    Update

---
+++ index.js
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+=======
+import { AppRegistry } from 'react-native';
+import App from './App';
+AppRegistry.registerComponent('DemoGA', () => App);
+>>>>>>> 0dcc8a6... Google analytics - update
